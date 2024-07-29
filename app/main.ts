dd39de8b34ffd77397e04a632352b7ec8f3f6ef2
Aaron Barnett <aaronsaccts@gmail.com>
2024-07-28 19:36:48 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,7 +8,17 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
-  socket.write("HTTP/1.1 200 OK\r\n\r\n")
+
+  socket.on('data', (data) => {
+    const message = data.toString()
+    const [requestLine, _headers] = message.split('\r\n')
+    const [_method, target, _version] = requestLine.split(' ')
+    if (target === '/') {
+      socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"))
+    } else {
+      socket.write(Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n"))
+    }
+  })
 });
 
 server.listen(4221, "localhost");
